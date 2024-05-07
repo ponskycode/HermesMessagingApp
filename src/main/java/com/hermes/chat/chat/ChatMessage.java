9275da8a68486f81af2e49f3aa94b119b264cde1
Michael Sz <99846294+ponskycode@users.noreply.github.com>
2024-05-08 01:13:48 +0200

    Skeleton for the app
    Problems with lombok library, needs to be replaced.

---
+++ src/main/java/com/hermes/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.hermes.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/hermes/chat/chat/MessageType.java
@@ -0,0 +1,7 @@
+package com.hermes.chat.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+}
