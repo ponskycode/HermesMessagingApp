6658348c1c307117bdd1deffaa772c4fe03dc0e1
omnom51 <166365002+omnom51@users.noreply.github.com>
2024-05-15 03:48:34 +0200

    Update chat-page.component.ts

--- src/main/resources/static/client/src/app/chat-page/chat-page.component.ts
+++ src/main/resources/static/client/src/app/chat-page/chat-page.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../chat.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat-page',
-  standalone: true,
-  imports: [],
   templateUrl: './chat-page.component.html',
-  styleUrl: './chat-page.component.css'
+  styleUrls: ['./chat-page.component.css'],
+  standalone: true,
+  imports: [FormsModule]
 })
 export class ChatPageComponent {
+  messages: string[] = [];
+  message: string = '';
+  connecting: boolean = true;
 
+  constructor(private chatService: ChatService) {
+    this.chatService.connect('username', (message: string) => {
+      this.connecting = false;
+      this.messages.push(message);
+    });
+  }
+
+  sendMessage() {
+    this.chatService.sendMessage(this.message);
+    this.message = '';
+  }
 }
