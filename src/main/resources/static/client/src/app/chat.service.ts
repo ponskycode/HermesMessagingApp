e090de86abe8c6fd9143ddec65ee56a001c48617
omnom51 <166365002+omnom51@users.noreply.github.com>
2024-05-15 03:53:00 +0200

    Update chat.service.ts

--- src/main/resources/static/client/src/app/chat.service.ts
+++ src/main/resources/static/client/src/app/chat.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import * as SockJS from 'sockjs-client';
+import * as Stomp from 'stompjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
+  private stompClient: any;
+  private messageCallback: (message: string) => void;
 
-  constructor() { }
+  connect(username: string, onMessageReceived: (message: string) => void) {
+    const socket = new SockJS('/ws');
+    this.stompClient = Stomp.over(socket);
+    this.messageCallback = onMessageReceived;
+
+    this.stompClient.connect({}, (frame: any) => {
+      console.log('Connected: ' + frame);
+      this.stompClient.subscribe('/topic/public', (message: any) => {
+        onMessageReceived(message.body);
+      });
+      this.stompClient.send('/app/chat.addUser', {}, JSON.stringify({ sender: username, type: 'JOIN' }));
+    }, (error: any) => {
+      console.error('Error: ' + error);
+    });
+  }
+
+  sendMessage(message: string) {
+    this.stompClient.send('/app/chat.sendMessage', {}, JSON.stringify({ content: message, type: 'CHAT' }));
+  }
 }
