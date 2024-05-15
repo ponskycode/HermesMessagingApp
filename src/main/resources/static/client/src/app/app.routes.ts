506af2b9445433fae57067e9772422c5c8db3fcc
omnom51 <166365002+omnom51@users.noreply.github.com>
2024-05-15 03:52:30 +0200

    Update app.routes.ts

--- src/main/resources/static/client/src/app/app.routes.ts
+++ src/main/resources/static/client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { provideRouter } from '@angular/router';
+import { UsernamePageComponent } from './username-page/username-page.component';
+import { ChatPageComponent } from './chat-page/chat-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: UsernamePageComponent },
+  { path: 'chat', component: ChatPageComponent }
+];
+
+export const appRoutingProviders = [
+  provideRouter(routes)
+];
