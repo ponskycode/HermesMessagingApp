703549119ad28591344b8f4255971a2f8424b383
omnom51 <166365002+omnom51@users.noreply.github.com>
2024-05-15 03:46:10 +0200

    Update main.ts

--- src/main/resources/static/client/src/main.ts
+++ src/main/resources/static/client/src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { appRoutingProviders } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(FormsModule),
+    ...appRoutingProviders
+  ]
+}).catch(err => console.error(err));
